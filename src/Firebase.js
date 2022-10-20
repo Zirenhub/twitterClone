961d6a15865b89fdcf7270c66fae25e6b9b9ffe5
Erdinch <erdinchorhan02@gmail.com>
2022-10-20 22:01:21 +0300

    firebase auth

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCTVwCY9nKM-z__ZB5NRjvm1nYWr-I9TlY',
@@ -15,5 +16,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export { app };
+export { app, auth };
