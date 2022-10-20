21ce5b921408babc930e7253663fbcb6e3f733b3
Erdinch <erdinchorhan02@gmail.com>
2022-10-20 22:02:23 +0300

    Sign In building

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -17,16 +17,23 @@ import appleIcon from '../../assets/images/154870.png';
 import twitterLogo from '../../assets/images/iconmonstr-twitter-1.svg';
 import { useState } from 'react';
 import SignUp from './SignUp';
+import SignIn from './SignIn';
 
 const WelcomePage = () => {
   const [showSignUp, setShowSignUp] = useState(false);
+  const [showSignIn, setShowSignIn] = useState(false);
 
   const handleShowSignUp = () => {
     setShowSignUp(true);
   };
 
+  const handleShowSignIn = () => {
+    setShowSignIn(true);
+  };
+
   const handleClose = () => {
     setShowSignUp(false);
+    setShowSignIn(false);
   };
 
   const Welcome = () => {
@@ -51,7 +58,7 @@ const WelcomePage = () => {
             <ExisitngAccountTitle>
               Already have an account?
             </ExisitngAccountTitle>
-            <Button bg="transparent" color="#1d9bf0">
+            <Button bg="transparent" color="#1d9bf0" onClick={handleShowSignIn}>
               Sign In
             </Button>
           </ExistingAccountContainer>
@@ -66,7 +73,9 @@ const WelcomePage = () => {
         <img src={twitterLogo} alt="twitter logo"></img>
       </SideImage>
       <MainContent>
-        {showSignUp ? <SignUp handleClose={handleClose}></SignUp> : Welcome()}
+        {showSignUp && <SignUp handleClose={handleClose}></SignUp>}
+        {showSignIn && <SignIn handleClose={handleClose}></SignIn>}
+        {!showSignUp && !showSignIn && Welcome()}
       </MainContent>
     </MainContainer>
   );
