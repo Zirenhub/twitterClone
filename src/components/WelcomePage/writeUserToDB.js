ebc97cbecdbdf6882b9b288e1b75ee6aba826531
Erdinch <erdinchorhan02@gmail.com>
2022-10-26 17:19:52 +0300

    refactored to use firestore timestamp

--- src/components/WelcomePage/writeUserToDB.js
+++ src/components/WelcomePage/writeUserToDB.js
@@ -1,34 +1,18 @@
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
 import { v4 as uuidv4 } from 'uuid';
 import { db } from '../../Firebase';
 
 const writeUserToDB = async (userID, displayName) => {
   const key = uuidv4();
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth();
-
-  const months = {
-    0: 'January',
-    1: 'February',
-    2: 'March',
-    3: 'April',
-    4: 'May',
-    5: 'June',
-    6: 'July',
-    7: 'August',
-    8: 'September',
-    9: 'October',
-    10: 'November',
-    11: 'December',
-  };
 
   try {
     await setDoc(doc(db, 'users', userID), {
       userName: displayName,
       key: key,
-      joinDate: `${months[month]} ${year}`,
+      firestoreDate: serverTimestamp(),
       tweetsNum: 0,
+      followers: {},
+      following: {},
     });
   } catch (error) {
     console.log(error);
