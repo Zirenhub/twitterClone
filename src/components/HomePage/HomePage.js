09da3bccd38474c4b92810eb93f3fcba65e6e56b
Erdinch <erdinchorhan02@gmail.com>
2022-10-28 16:49:40 +0300

    fixed sorting tweets by date

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,9 +1,7 @@
 import {
   HomepageHeader,
-  HomepageMain,
   HomepageSignout,
   HomepageTestPP,
-  HomepageTweetsContainer,
   HomepageWriteTweet,
 } from '../../styles/HomePageStyles/HomePage.styled';
 import { UserAuth } from '../../context/authContext';
@@ -25,6 +23,7 @@ import {
   ProfileCommentButton,
   ProfileLikeButton,
   ProfileRetweetButton,
+  ProfileMain,
 } from '../../styles/ProfilePageStyles/ProfilePage.styled';
 import commentIcon from '../../assets/images/comment-svgrepo-com.svg';
 import retweetIcon from '../../assets/images/retweet-svgrepo-com.svg';
@@ -73,90 +72,91 @@ const HomePage = () => {
   }, []);
 
   useEffect(() => {
+    const tweetsArr = [];
+
     allTweets.forEach((userTweets) => {
       userTweets.forEach((tweet) => {
         const convertToArray = Object.entries(tweet);
-        setMergedData((prevVal) => [...prevVal, convertToArray]);
+        tweetsArr.push(convertToArray);
       });
     });
-  }, [allTweets]);
 
-  useEffect(() => {
-    const sortedTweets = mergedData.sort((a, b) => {
+    const sortedTweets = tweetsArr.sort((a, b) => {
       return new Date(b[0][1].firestoreDate) - new Date(a[0][1].firestoreDate);
     });
+
     setMergedData(sortedTweets);
-  }, [mergedData]);
+  }, [allTweets]);
 
   return (
     <>
       {loading ? (
         <LoadingStyled>Loading</LoadingStyled>
       ) : (
-        <HomepageMain>
-          <HomepageHeader>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <HomepageTestPP onClick={handleSwitchToProfile}></HomepageTestPP>
-              <div style={{ marginLeft: 15 }}>
-                <p style={{ fontSize: 15, fontWeight: 'bold' }}>
-                  Latest Tweets
-                </p>
+        <ProfileMain>
+          <HomepageWriteTweet onClick={navigateToTweet}>
+            <img src={tweetButton} alt="write tweet button"></img>
+          </HomepageWriteTweet>
+          <ProfileTweetFeedContainer>
+            <HomepageHeader>
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <HomepageTestPP
+                  onClick={handleSwitchToProfile}
+                ></HomepageTestPP>
+                <div style={{ marginLeft: 15 }}>
+                  <p style={{ fontSize: 15, fontWeight: 'bold' }}>
+                    Latest Tweets
+                  </p>
+                </div>
               </div>
-            </div>
 
-            <HomepageSignout onClick={handleLogout}>
-              <img src={signOut} alt="sign out button"></img>
-            </HomepageSignout>
-          </HomepageHeader>
-          <HomepageTweetsContainer>
-            <HomepageWriteTweet onClick={navigateToTweet}>
-              <img src={tweetButton} alt="write tweet button"></img>
-            </HomepageWriteTweet>
-            <ProfileTweetFeedContainer>
-              {mergedData &&
-                mergedData.map((tweet) => {
-                  return (
-                    <ProfileTweetContainer key={tweet[0][0]}>
-                      <div style={{ display: 'flex' }}>
-                        <HomepageTestPP
-                          style={{
-                            border: '1px solid red',
-                            backgroundColor: '#ffffff',
-                            minHeight: 48,
-                            minWidth: 48,
-                            flexGrow: 1,
-                          }}
-                        ></HomepageTestPP>
-                        <ProfileTweetContent>
-                          <div style={{ display: 'flex' }}>
-                            <ProfileWhiteBold>
-                              {tweet[1][1].userName}
-                            </ProfileWhiteBold>
-                            <ProfileGrayText style={{ marginLeft: 10 }}>
-                              {tweet[0][1].firestoreDate.slice(0, 21)}
-                            </ProfileGrayText>
-                          </div>
+              <HomepageSignout onClick={handleLogout}>
+                <img src={signOut} alt="sign out button"></img>
+              </HomepageSignout>
+            </HomepageHeader>
+            {mergedData &&
+              mergedData.map((tweet) => {
+                return (
+                  <ProfileTweetContainer key={tweet[0][0]}>
+                    <div style={{ display: 'flex' }}>
+                      <HomepageTestPP
+                        style={{
+                          border: '1px solid red',
+                          backgroundColor: '#ffffff',
+                          minHeight: 48,
+                          minWidth: 48,
+                          flexGrow: 1,
+                        }}
+                      ></HomepageTestPP>
+                      <ProfileTweetContent>
+                        <div style={{ display: 'flex' }}>
+                          <ProfileWhiteBold>
+                            {tweet[1][1].userName}
+                          </ProfileWhiteBold>
+                          <ProfileGrayText style={{ marginLeft: 10 }}>
+                            {tweet[0][1].firestoreDate.slice(0, 21)}
+                          </ProfileGrayText>
+                        </div>
 
-                          <ProfileWhite>{tweet[0][1].tweet}</ProfileWhite>
-                        </ProfileTweetContent>
-                      </div>
+                        <ProfileWhite>{tweet[0][1].tweet}</ProfileWhite>
+                      </ProfileTweetContent>
+                    </div>
 
-                      <ProfileTweetInteractContainer>
-                        <ProfileCommentButton
-                          src={commentIcon}
-                        ></ProfileCommentButton>
-                        <ProfileRetweetButton
-                          src={retweetIcon}
-                        ></ProfileRetweetButton>
-                        <ProfileLikeButton src={likeIcon}></ProfileLikeButton>
-                      </ProfileTweetInteractContainer>
-                    </ProfileTweetContainer>
-                  );
-                })}
-            </ProfileTweetFeedContainer>
-          </HomepageTweetsContainer>
+                    <ProfileTweetInteractContainer>
+                      <ProfileCommentButton
+                        src={commentIcon}
+                      ></ProfileCommentButton>
+                      <ProfileRetweetButton
+                        src={retweetIcon}
+                      ></ProfileRetweetButton>
+                      <ProfileLikeButton src={likeIcon}></ProfileLikeButton>
+                    </ProfileTweetInteractContainer>
+                  </ProfileTweetContainer>
+                );
+              })}
+          </ProfileTweetFeedContainer>
           <Footer></Footer>
-        </HomepageMain>
+        </ProfileMain>
       )}
     </>
   );
