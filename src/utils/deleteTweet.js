03ceed9b292461fb5895e0de4dd27fd0892e0f49
Erdinch <erdinchorhan02@gmail.com>
2022-11-06 18:45:25 +0200

    delete tweet AND it's replies

--- src/utils/deleteTweet.js
+++ src/utils/deleteTweet.js
@@ -1,14 +1,22 @@
-import { doc, deleteField, updateDoc, increment } from 'firebase/firestore';
+import {
+  doc,
+  deleteField,
+  updateDoc,
+  increment,
+  deleteDoc,
+} from 'firebase/firestore';
 import { db } from '../Firebase';
 
 const deleteTweet = async (user, tweet) => {
   const tweetRef = doc(db, 'posts', user);
+  const repliesRef = doc(tweetRef, 'replies', tweet);
   const userInfoRef = doc(db, 'users', user);
 
   try {
     await updateDoc(tweetRef, {
       [tweet]: deleteField(),
     });
+    await deleteDoc(repliesRef);
     await updateDoc(userInfoRef, {
       tweetsNum: increment(-1),
     });
