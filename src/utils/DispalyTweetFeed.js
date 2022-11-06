a4308ce900db4e144f04d1608e9450a4e722bdcd
Erdinch <erdinchorhan02@gmail.com>
2022-11-06 18:45:57 +0200

    refactored display tweets feed

--- src/utils/DispalyTweetFeed.js
+++ src/utils/DispalyTweetFeed.js
@@ -1,46 +1,32 @@
 import { UserAuth } from '../context/authContext';
-import { useState } from 'react';
-import { useEffect } from 'react';
+
 import DisplaySingleTweet from './DisplaySingleTweet';
 import deleteTweet from './deleteTweet';
 
 const DispalyTweetFeed = (props) => {
-  const { initialTweets } = props;
+  const { initialTweets, setTweets } = props;
   const { user } = UserAuth();
 
-  // doing this to prevent fetching data everytime user deletes a tweet,
-  // this way we just delete the tweet from the tweet array,
-  // updating the tweet feed only with the remaining tweets.
-  const [tweets, setTweets] = useState(initialTweets);
-
-  useEffect(() => {
-    setTweets(initialTweets);
-  }, [initialTweets]);
-
-  const handleDeleteTweet = (key) => {
-    const initialize = async () => {
-      try {
-        await deleteTweet(user.uid, key);
-        const updatedTweets = tweets.filter((tweet) => {
-          return tweet.key !== key;
-        });
-        setTweets(updatedTweets);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    initialize();
+  const handleDeleteTweet = async (key) => {
+    try {
+      await deleteTweet(user.uid, key);
+      const updatedTweets = initialTweets.filter((tweet) => {
+        return tweet.key !== key;
+      });
+      setTweets(updatedTweets);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <>
-      {tweets.map((tweet) => {
+      {initialTweets.map((tweet) => {
         return (
           <DisplaySingleTweet
             key={tweet.key}
             tweetLink={tweet.key}
             tweet={tweet}
-            user={user}
             handleDeleteTweet={() => handleDeleteTweet(tweet.key)}
           ></DisplaySingleTweet>
         );
