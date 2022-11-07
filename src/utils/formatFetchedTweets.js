28931efe0131f41248a4847d3bce3cd7a2fef7b7
Erdinch <erdinchorhan02@gmail.com>
2022-11-07 22:23:40 +0200

    refactored

--- src/utils/formatFetchedTweets.js
+++ src/utils/formatFetchedTweets.js
@@ -6,6 +6,8 @@ const formatFetchedTweets = (tweetsArr) => {
   const numLikes = tweetsArr[1].numberOfLikes;
   const numRetweets = tweetsArr[1].numberOfRetweets;
   const date = tweetsArr[1].firestoreDate.toDate();
+  const replyingTo = tweetsArr[1].replyingTo;
+  const user = tweetsArr[1].user;
 
   return {
     key: key,
@@ -14,6 +16,8 @@ const formatFetchedTweets = (tweetsArr) => {
     numLikes: numLikes,
     numRetweets: numRetweets,
     date: date,
+    replyingTo: replyingTo,
+    user: user,
   };
 };
 
