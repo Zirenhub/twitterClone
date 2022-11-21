1befd547b39ce8d19995e8e55966ad7d9bef2473
Erdinch <erdinchorhan02@gmail.com>
2022-11-21 20:09:16 +0200

    deleted

--- src/components/MessagesPage/getMessages.js
+++
@@ -1,9 +0,0 @@
-import { collection, onSnapshot, query, where } from 'firebase/firestore';
-import { db } from '../../Firebase';
-
-const getMessages = async (profileID, userID, setChatMessages) => {
-  if (profileID && userID) {
-  }
-};
-
-export default getMessages;
